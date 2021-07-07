a65aefc9c35705323d2d4b9ed0d96c23095529f2
rudgus <rudgus4620@gmail.com>
2021-07-07 16:41:51 +0900

    exchange-rate

---
+++ main.js
@@ -0,0 +1,38 @@
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+function calculate() {
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+  fetch("https://open.exchangerate-api.com/v6/latest")// 이거 환율  API.여
+
+  //솔직히 나라안에 값을 가져오기 위해서 json 쓴것 같긴 한데 저러면 그냥 .then안에 써서 적는게  훨씬 정확한거 아닌가?? 
+    .then(res => res.json())
+    .then(data => {
+      //  console.log(data);
+      const rate = data.rates[currency_two] / data.rates[currency_one];
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+      amountEl_two.value = (amountEl_one.value * (rate)).toFixed(2);
+    });
+}
+
+
+// Event Listener
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+
+swap.addEventListener('click', () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  calculate();
+});
+
+
+calculate();
